003188548f04ecca447139f81cbfb502d7f26443
Anthony Sottile <asottile@umich.edu>
2021-12-01 00:24:07 -0500

    implement day 1 in sqlite

---
+++ day01/part2.sqlite.sql
@@ -0,0 +1,31 @@
+-- our puzzle input
+CREATE TABLE input (value STRING);
+INSERT INTO input VALUES (TRIM(readfile('input.txt'), char(10)));
+
+CREATE TABLE numbers (n INT);
+WITH RECURSIVE
+    nn (n, rest)
+AS (
+    SELECT
+        (SELECT SUBSTR(input.value, 0, INSTR(input.value, char(10))) FROM input),
+        (SELECT SUBSTR(input.value, INSTR(input.value, char(10)) + 1) FROM input)
+    UNION ALL
+    SELECT
+        CASE INSTR(nn.rest, char(10))
+            WHEN 0 THEN nn.rest
+            ELSE SUBSTR(nn.rest, 0, INSTR(nn.rest, char(10)))
+        END,
+        CASE INSTR(nn.rest, char(10))
+            WHEN 0 THEN ''
+            ELSE SUBSTR(nn.rest, INSTR(nn.rest, char(10)) + 1)
+        END
+    FROM nn
+    WHERE LENGTH(nn.rest) > 0
+)
+INSERT INTO numbers (n)
+SELECT nn.n FROM nn;
+
+SELECT SUM(numbers1.n > numbers2.n)
+FROM numbers AS numbers1
+INNER JOIN numbers as numbers2
+WHERE numbers1.ROWID = numbers2.ROWID + 3;
