2769e4536fa5e710a2ab6d7a47976aa445ec4158
Anthony Sottile <asottile@umich.edu>
2021-12-01 12:21:30 -0500

    improved sqlite solutions using json extension

--- day01/part1.sqlite.sql
+++ day01/part1.sqlite.sql
@@ -1,29 +1,11 @@
 -- our puzzle input
-CREATE TABLE input (value STRING);
+CREATE TABLE input (s STRING);
 INSERT INTO input VALUES (TRIM(readfile('input.txt'), char(10)));
 
-CREATE TABLE numbers (n INT);
-WITH RECURSIVE
-    nn (n, rest)
-AS (
-    SELECT
-        (SELECT SUBSTR(input.value, 0, INSTR(input.value, char(10))) FROM input),
-        (SELECT SUBSTR(input.value, INSTR(input.value, char(10)) + 1) FROM input)
-    UNION ALL
-    SELECT
-        CASE INSTR(nn.rest, char(10))
-            WHEN 0 THEN nn.rest
-            ELSE SUBSTR(nn.rest, 0, INSTR(nn.rest, char(10)))
-        END,
-        CASE INSTR(nn.rest, char(10))
-            WHEN 0 THEN ''
-            ELSE SUBSTR(nn.rest, INSTR(nn.rest, char(10)) + 1)
-        END
-    FROM nn
-    WHERE LENGTH(nn.rest) > 0
-)
-INSERT INTO numbers (n)
-SELECT nn.n FROM nn;
+CREATE TABLE numbers(n INT);
+INSERT INTO numbers
+SELECT t.value
+FROM json_each('[' || REPLACE((SELECT s FROM input), char(10), ',') || ']') t;
 
 SELECT SUM(numbers1.n > numbers2.n)
 FROM numbers AS numbers1
