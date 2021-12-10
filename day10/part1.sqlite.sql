27542605e554742d3dbc2e058e7438f614da10d8
Anthony Sottile <asottile@umich.edu>
2021-12-10 00:48:40 -0500

    improve part 1 in sqlite

--- day10/part1.sqlite.sql
+++ day10/part1.sqlite.sql
@@ -2,51 +2,46 @@
 CREATE TABLE input (s STRING);
 INSERT INTO input VALUES (TRIM(readfile('input.txt'), char(10)));
 
+CREATE TABLE braces (k, v, n);
+INSERT INTO braces VALUES
+    ('(', ')', 3),
+    ('[', ']', 57),
+    ('{', '}', 1197),
+    ('<', '>', 25137);
+
 WITH RECURSIVE nn (total, stack, rest) AS (
     SELECT 0, '', (SELECT s || char(10) FROM input)
     UNION ALL
     SELECT
         CASE
-            WHEN SUBSTR(nn.rest, 1, 1) = ')' AND SUBSTR(nn.stack, -1, 1) != '('
-            THEN nn.total + 3
-            WHEN SUBSTR(nn.rest, 1, 1) = ']' AND SUBSTR(nn.stack, -1, 1) != '['
-            THEN nn.total + 57
-            WHEN SUBSTR(nn.rest, 1, 1) = '}' AND SUBSTR(nn.stack, -1, 1) != '{'
-            THEN nn.total + 1197
-            WHEN SUBSTR(nn.rest, 1, 1) = '>' AND SUBSTR(nn.stack, -1, 1) != '<'
-            THEN nn.total + 25137
+            WHEN (
+                SUBSTR(nn.rest, 1, 1) IN (')', ']', '}', '>') AND
+                SUBSTR(nn.rest, 1, 1) != SUBSTR(nn.stack, -1, 1)
+            )
+            THEN (
+                nn.total +
+                (SELECT n FROM braces WHERE v = SUBSTR(nn.rest, 1, 1))
+            )
             ELSE nn.total
         END,
         CASE
             WHEN SUBSTR(nn.rest, 1, 1) = char(10) THEN ''
             WHEN SUBSTR(nn.rest, 1, 1) IN ('(', '[', '{', '<')
-            THEN nn.stack || SUBSTR(nn.rest, 1, 1)
-            WHEN SUBSTR(nn.rest, 1, 1) = ')' AND SUBSTR(nn.stack, -1, 1) = '('
+            THEN (
+                nn.stack ||
+                (SELECT v FROM braces WHERE k = SUBSTR(nn.rest, 1, 1))
+            )
+            WHEN SUBSTR(nn.rest, 1, 1) = SUBSTR(nn.stack, -1, 1)
             THEN SUBSTR(nn.stack, 1, LENGTH(nn.stack) - 1)
-            WHEN SUBSTR(nn.rest, 1, 1) = ']' AND SUBSTR(nn.stack, -1, 1) = '['
-            THEN SUBSTR(nn.stack, 1, LENGTH(nn.stack) - 1)
-            WHEN SUBSTR(nn.rest, 1, 1) = '}' AND SUBSTR(nn.stack, -1, 1) = '{'
-            THEN SUBSTR(nn.stack, 1, LENGTH(nn.stack) - 1)
-            WHEN SUBSTR(nn.rest, 1, 1) = '>' AND SUBSTR(nn.stack, -1, 1) = '<'
-            THEN SUBSTR(nn.stack, 1, LENGTH(nn.stack) - 1)
-            WHEN SUBSTR(nn.rest, 1, 1) = ')' AND SUBSTR(nn.stack, -1, 1) != '('
-            THEN ''
-            WHEN SUBSTR(nn.rest, 1, 1) = ']' AND SUBSTR(nn.stack, -1, 1) != '['
-            THEN ''
-            WHEN SUBSTR(nn.rest, 1, 1) = '}' AND SUBSTR(nn.stack, -1, 1) != '{'
-            THEN ''
-            WHEN SUBSTR(nn.rest, 1, 1) = '>' AND SUBSTR(nn.stack, -1, 1) != '<'
+            WHEN SUBSTR(nn.rest, 1, 1) != SUBSTR(nn.stack, -1, 1)
             THEN ''
             ELSE 'WAT?'
         END,
         CASE
-            WHEN SUBSTR(nn.rest, 1, 1) = ')' AND SUBSTR(nn.stack, -1, 1) != '('
-            THEN SUBSTR(nn.rest, INSTR(nn.rest, char(10)) + 1)
-            WHEN SUBSTR(nn.rest, 1, 1) = ']' AND SUBSTR(nn.stack, -1, 1) != '['
-            THEN SUBSTR(nn.rest, INSTR(nn.rest, char(10)) + 1)
-            WHEN SUBSTR(nn.rest, 1, 1) = '}' AND SUBSTR(nn.stack, -1, 1) != '{'
-            THEN SUBSTR(nn.rest, INSTR(nn.rest, char(10)) + 1)
-            WHEN SUBSTR(nn.rest, 1, 1) = '>' AND SUBSTR(nn.stack, -1, 1) != '<'
+            WHEN (
+                SUBSTR(nn.rest, 1, 1) IN (')', ']', '}', '>') AND
+                SUBSTR(nn.rest, 1, 1) != SUBSTR(nn.stack, -1, 1)
+            )
             THEN SUBSTR(nn.rest, INSTR(nn.rest, char(10)) + 1)
             ELSE SUBSTR(nn.rest, 2)
         END
