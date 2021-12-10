9f1e02b7f3b228958befbdf13b153e6e66a0761f
Anthony Sottile <asottile@umich.edu>
2021-12-10 00:10:55 -0500

    day 10 part 1

--- README.md
+++ README.md
@@ -81,4 +81,7 @@ $ find -maxdepth 1 -type d -name 'day*' -not -name day00 | sort | xargs --replac
 + python day09/part2.py day09/input.txt
 931200
 > 46183 μs
++ python day10/part1.py day10/input.txt
+316851
+> 2531 μs
 ```
---
+++ day10/part1.py
@@ -0,0 +1,76 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+from support import timing
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+POINTS = {
+    ')': 3,
+    ']': 57,
+    '}': 1197,
+    '>': 25137,
+}
+FORWARD = {'{': '}', '(': ')', '[': ']', '<': '>'}
+REVERSE = {v: k for k, v in FORWARD.items()}
+
+
+def compute(s: str) -> int:
+    total = 0
+    lines = s.splitlines()
+    for line in lines:
+        stack = []
+        for c in line:
+            if c in FORWARD:
+                stack.append(c)
+            elif c in REVERSE:
+                if stack[-1] != REVERSE[c]:
+                    total += POINTS[c]
+                    break
+                else:
+                    stack.pop()
+    return total
+
+
+INPUT_S = '''\
+[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]
+'''
+EXPECTED = 26397
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
